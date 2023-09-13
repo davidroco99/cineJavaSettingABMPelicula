3cc59901beee8537d22f8204cef5ba929847c277
David Roco <davidroco99@gmail.com>
2023-09-13 12:56:57 -0300

    Add files via upload

---
+++ target/generated-sources/annotations/com/programacion2/cine/logica/EstadoFuncion_.java
@@ -0,0 +1,14 @@
+package com.programacion2.cine.logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-09-12T21:37:45")
+@StaticMetamodel(EstadoFuncion.class)
+public class EstadoFuncion_ { 
+
+    public static volatile SingularAttribute<EstadoFuncion, String> nombreEstadoFunción;
+    public static volatile SingularAttribute<EstadoFuncion, Integer> idEstadoFuncion;
+
+}
