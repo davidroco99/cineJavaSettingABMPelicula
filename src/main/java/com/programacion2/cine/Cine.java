6484a3bb264da713bd6cf7b9191076737754b801
David Roco <davidroco99@gmail.com>
2023-09-13 12:50:29 -0300

    Add files via upload

---
+++ src/main/java/com/programacion2/cine/Cine.java
@@ -0,0 +1,18 @@
+
+package com.programacion2.cine;
+
+import com.programacion2.cine.igu.MenuInicial;
+
+/**
+ *
+ * @author DavidRoco
+ */
+public class Cine {
+
+    public static void main(String[] args) {
+       
+        MenuInicial pantallaInicial = MenuInicial.getInstance();
+        pantallaInicial.setVisible(true);
+        pantallaInicial.setLocationRelativeTo(null);
+    }
+}
